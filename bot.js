0da10a7925ec892e760c6168c5798491125911fa
EtherAce <65919120+EtherAce@users.noreply.github.com>
2020-05-25 14:42:41 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+{
+"token":"NzE0MzAyNDg2OTk4OTQxODA3.XsssRA.hUjSNET7mUoec-l50zP0km9WZ-w",
+"prefix": "e!"
+}
+
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+
+//online status
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!` )
+    bot.user.setActivity("My prefix is e!", {type: "PLAYING"});
+})
+
+
+bot.on("message", async message => {
+if(message.author.bot || message.channel.type === "dm") return;
+
+let prefix = botconfig.prefix;
+let messageArray = message.content.split(" ")
+let cmd = messageArray[0];
+let args = messageArray.slice[0];
+
+client.login(process.env.BOT_TOKEN);
